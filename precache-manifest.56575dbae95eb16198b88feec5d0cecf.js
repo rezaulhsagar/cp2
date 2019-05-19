9039feba305a36a5daf3dbcfbf7fa9b554ebb763
raihanul-2k15 <refat0167@gmail.com>
2019-05-19 20:09:10 +0600

    added actual files
    same jinish re bhai

---
+++ precache-manifest.56575dbae95eb16198b88feec5d0cecf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63e642c006ef533a9fab1f57cc06336a",
+    "url": "/cp2/index.html"
+  },
+  {
+    "revision": "2eeaf7e39cd72e7a530d",
+    "url": "/cp2/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f445504e28b191a42334",
+    "url": "/cp2/static/js/2.df6606ae.chunk.js"
+  },
+  {
+    "revision": "2eeaf7e39cd72e7a530d",
+    "url": "/cp2/static/js/main.7b0a2732.chunk.js"
+  },
+  {
+    "revision": "977f5b24b3d05631e70c",
+    "url": "/cp2/static/js/runtime~main.49fbbdd6.js"
+  }
+]);
